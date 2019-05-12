99719c78d3206a2e5d935cc32171ca3c595d9923
Benjamin Rouquet <benjamin.rouquet4@gmail.com>
2019-05-12 12:09:58 +0200

    Mise en place basique de vue particle js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import App from './App'
 import router from './router'
 import VueParticles from 'vue-particles'
 
-        
+Vue.use(VueParticles)
+
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
-})
-
-Vue.use(VueParticles)
+});
