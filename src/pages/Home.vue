7313133a7ce898ccc65516d56630918df441ed9d
Benjamin Rouquet <benjamin.rouquet4@gmail.com>
2019-05-12 12:57:42 +0200

    Préermier design header

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,26 +1,25 @@
 <template>
     <div id="app">
-  <vue-particles
-        color="#000"
-        :particleOpacity="0.7"
-        :particlesNumber="80"
-        shapeType="circle"
-        :particleSize="4"
-        linesColor="#000"
-        :linesWidth="1"
-        :lineLinked="true"
-        :lineOpacity="0.4"
-        :linesDistance="150"
-        :moveSpeed="2"
-        :hoverEffect="false"
-        hoverMode="grab"
-        :clickEffect="false"
-        clickMode="push"
-      >
-      
-  </vue-particles>
-        <cover name="Benjamin Rouquet"></cover>
-        <skills></skills>
+      <cover name="Benjamin Rouquet"></cover>
+
+      <vue-particles
+            color="#20364d"
+            :particleOpacity="0.7"
+            :particlesNumber="60"
+            shapeType="circle"
+            :particleSize="4"
+            linesColor="#1b4f6d"
+            :linesWidth="1"
+            :lineLinked="true"
+            :lineOpacity="0.4"
+            :linesDistance="150"
+            :moveSpeed="2"
+            :hoverEffect="false"
+            hoverMode="grab"
+            :clickEffect="false"
+            clickMode="push"
+          >
+        </vue-particles>
     </div>
 </template>
 
